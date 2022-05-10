2f78eab6ab105cd3d05d29009deb1713cbc379d0
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-10 09:54:26 +0100

    Get the photos from the API call and display in the Gallery component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import apiKey from './config.js';
+import axios from 'axios';
+
+import SearchForm from './components/SearchForm';
+import Navbar from './components/Navbar';
+import Gallery from './components/Gallery';
 
 class App extends Component {
-    render() {
-        return ( <
-            div className = "App" >
-            <
-            header className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            h1 className = "App-title" > Welcome to React < /h1> <
-            /header> <
-            p className = "App-intro" >
-            To get started, edit < code > src / App.js < /code> and save to reload. <
-            /p> <
-            /div>
-        );
-    }
-}
 
-export default App;
+    state = {
+        photos: [],
+        tag: 'farm life'
+    };
+
+
+    componentDidMount() {
+        // this is asynchronous and returns a promise
+        // .then() method only runs when the get request is completed.
+        // pass a callback function to the .then() method which takes the response object
+        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&media=photo&tags=${this.state.tag}&per_page=10&format=json&nojsoncallback=1`)
+            .then(res => {
+                // console.log(res.data.photos.photo);
+                this.setState({
+                    photos: res.data.photos.photo
+                });
+            });
+
+    }
+
+    // create a function to accept the search term
+    searchTags = (searchTermObj) => {
+        this.setState({
+            tag: searchTermObj.search
+        });
+
+        // UPDATE AND MAKE A NEW CALL TO THE API ENDPOINT
+
+    }
+
+    render() {
+        return (
+                    <div className="container">
+
+                    <SearchForm searchTags={this.searchTags} />
+                    <Navbar />
+                    <Gallery photos={this.state.photos} />
+
+                </div>
+                );
+  }
+}
+                export default App;
